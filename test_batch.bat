11df1bf44b1873d2160d4209a4105514d52f073c
PmaityTeo <Papiya.Maity@teoco.com>
2026-07-02 14:44:44 +0530

    Update test_batch.bat

--- test_batch.bat
+++ test_batch.bat
@@ -2,11 +2,11 @@
 
 set /a number=%1
 set /a n=4
-set /a mod=%n% %% 2
+set /a mod=%1 %% 2
 
 if %mod% EQU 0 (
-    echo "%n% is even"
+    echo "%1 is even"
     exit /b
 ) else (
-    echo "%n% is odd"
+    echo "%1 is odd"
 )
